6f69616649935ad12ccf1e9a6c98260e2b880e5e
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-02 12:06:15 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,17 @@
+# set2
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+double base,exp,res;
+printf("enter base no");
+scanf("%1f",&base);
+printf("enter exp no");
+scanf("%1f",&exp);
+while(exp!=0)
+{
+res=pow(base,exp);
+printf("%.11f ^%11f "=".21%f",base,exp,res);
+return 0;
+}
+}
