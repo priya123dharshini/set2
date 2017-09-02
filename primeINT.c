b45994f50ea90528e4c6a4e493d8dfc9a2177a16
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-02 22:16:27 +0530

    Rename prime.c to primeINT.c

---
+++ primeINT.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int i,j,m,n,flag;
+printf("enter the two values");
+scanf("%d", %d",&n,&m);
+printf("prime no btween %d and %d are:"&n,&m);
+for(i=n;i<m;++i)
+{
+   flag=0;
+   for(j=2;j<=i/2;++j)
+   {
+     if(i/j==1)
+     {
+     flag=1;
+     break;
+     }
+     if(flag==0)
+     {
+     printf("%d",i);
+     }
+   }
+  }
+}
+
+     
+     
+
