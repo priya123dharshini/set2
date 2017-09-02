d7c97aa485f54f158a816b14e3073b68c301f47d
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-02 21:42:55 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,m,i;
+printf("enter n value"):
+scanf("%d",&n);
+printf("enter m value");
+scanf("%d",&m);
+for(i=n;i<=m;i++)
+{
+ if(i %2==1)
+ {
+  printf("the numbers are",i);
+ }
+}
+
