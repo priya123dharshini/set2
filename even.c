6976d2c4da3a8cd5313a7c43bca23c8b0dc14fd5
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-02 21:50:23 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,m,i;
+printf("enter n value"):
+scanf("%d",&n);
+printf("enter m value");
+scanf("%d",&m);
+for(i=n;i<=m;i++)
+{
+ if(i %2==0)
+ {
+  printf("the numbers are",i);
+ }
