71d011b6509ab89bfecec36faf62fdb70c406880
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-03 12:03:15 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,19 @@
+#inclide <stdio.h>
+int main ()
+{
+int nu,or,re,re=0;
+printf ("enter a three digit integer:");
+scanf("%d",&number);
+or=nu;
+while(or:=0)
+{
+remainder =or%10;
+result +=remaider*remainder*remaider;
+or/=10;
+}
+if(result==nu)
+print("%d is an armstrong nu.", nu);
+else
+printf("%d is not an armstrong nu.",nu);
+}
+return 0;
