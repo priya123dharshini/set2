0ba4370cd130f01e41e2e50906bef0812eb9fcd6
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-03 11:34:09 +0530

    Create amstrngint.cpp

---
+++ amstrngint.cpp
@@ -0,0 +1,24 @@
+#include <isotream>
+using namesapce std;
+int main()n5
+{
+int num1,num2,i,n,b,s;
+cout<<"enter 1 number";
+cin>> num2;
+cout <<"enter second number";
+cin>>num2;
+cout<<"armstrong number between" << num1 <<"and"
+<<num2<< " are " <<end1;
+for (i=num1; i<=num2; i++){
+s=0;
+n=i;
+for(n=i;n>0;n/=10){
+b=n%10;
+s=s+b*b*b;
+}
+if(s==i){
+cout <<i<< en1;
+}
+}
+return0;
+}
