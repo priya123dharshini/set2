dbd7c3f004aa0514d6bd859856e996534eaaa4a3
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-05 20:30:35 +0530

    exit.c

---
+++ exit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int char ch;
+int n;
+
+clrscr();
+void main(){
+
+printf("Print 1 to 5 again and again");
+while(1){
+for(n=1;n<=5;n++)
+
+     printf("\n%d",n);
+     ch=getch();
+     if(ch=='Q')
+     exit(0)
+   }
+
+    getch();
+    }
